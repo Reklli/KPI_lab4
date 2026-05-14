711d6220ba15213cf99cbe2c323394983c6a205b
Reklli <96347010+Reklli@users.noreply.github.com>
2026-05-14 19:26:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using ParkingSystem.Controllers;
+using ParkingSystem.Interfaces;
+using ParkingSystem.Services;
+using System;
+using System.Text
+
+namespace ParkingSystem;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+
+        IParkingRepository repository = new ParkingRepository(rows: 3, spotsPerRow: 5);
+        IPaymentService paymentService = new PaymentService();
+        IDisplayService display = new DisplayService();
+
+        IParkingController controller = new ParkingController(
+            repository, paymentService, display);
+
+        controller.Run();
+    }
+}
